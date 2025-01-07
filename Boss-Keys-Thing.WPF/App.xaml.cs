73e82304a3ca918bbc577402e2cf3048e27b8f70
Godofdrakes <GodofDrakes@pm.me>
2025-01-07 08:37:54 -0800

    Initial commit

---
+++ Boss-Keys-Thing.WPF/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Boss_Keys_Thing.WPF;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application { }
